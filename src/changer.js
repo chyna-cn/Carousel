84dfe78c73cf0171d7c8e07d525fbdfde3b68e47
Nora Nwachukwu <chinomso@Noras-MacBook-Air.local>
2023-02-13 12:58:26 +0100

    saving

--- src/changer.js
+++ src/changer.js
@@ -5,20 +5,22 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons'
 
 class Changer extends React.Component {
-    state = {Current: ImageList[0]};
+    state = {Current: 0 };
 
     render() {
         const Next = () => {
-            this.setState({Current = 0 ? ImageList.length - 1 : Current + 1 })
+            const newCurrent=this.state.Current=== ImageList.length-1 ? 0 :this.state.Current+1;
+            this.setState({ Current:newCurrent })
         }
         const Prev = () => {
-            this.setState({Current = 0 ? ImageList.length - 1 : Current - 1 })
+            const newCurrent=this.state.Current=== 0 ? ImageList.length-1:this.state.Current-1;
+            this.setState({ Current:newCurrent })
         }
         return (
             <div className='box'>
-                <FontAwesomeIcon icon={faArrowLeft} onClick={Prev}/>
-                <img src={this.state.Current} alt='pic'/>
-                <FontAwesomeIcon icon={faArrowRight} onClick={Next}/>
+                <FontAwesomeIcon icon={faArrowLeft} size = '6x' onClick={Prev}/>
+                <img src={ImageList[this.state.Current]} alt='pic'/>
+                <FontAwesomeIcon icon={faArrowRight} size = '6x' onClick={Next}/>
             </div>
         )
     }
